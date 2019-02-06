f75f51444f59d28a424a813f7cefeefc3550c767
Daniel Hernández <danielht97-@outlook.com>
2019-02-06 09:06:04 -0600

    Se añadio otra prueba

--- testCalculadora.py
+++ testCalculadora.py
@@ -3,9 +3,14 @@ from calculadora import Calculadora
 
 class TestCalculadora(unittest.TestCase):
 
-	def test_suma_2_mas_dos(self):
+	def test_suma_dos_mas_dos(self):
 		calc = Calculadora()
 		self.assertEquals(4, calc.suma(2, 2))
+
+	def test_suma_dos_mas_tres(self):
+		calc = Calculadora()
+		self.assertEquals(5, calc.suma(2, 3))
+
 
 if __name__ == '__main__':
     unittest.main()
